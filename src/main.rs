de74788349f57c7d32d68fbce62bf6dbbeea0753
Alexis "Horgix" Chotard <alexis.horgix.chotard@gmail.com>
2022-06-17 23:36:56 +0200

    ✨ feat(main): add proper error handling (and messages) with `anyhow`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use octocrab::Octocrab;
 use serde::{de,Serialize, Deserialize, Deserializer};
+use anyhow::{Context, Result};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PullRequestsMetrics {
@@ -27,16 +28,19 @@ fn deserialize_nested_total_count<'de, D: Deserializer<'de>>(deserializer: D) ->
 }
 
 #[tokio::main]
-async fn main() -> octocrab::Result<()> {
-    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+async fn main() -> anyhow::Result<()> {
+    let token = std::env::var("GITHUB_TOKEN").expect("environment variable 'GITHUB_TOKEN' is required");
 
     // Bootstrap an authenticated GitHub client
-    let github_client = Octocrab::builder().personal_token(token).build()?;
+    let github_client = Octocrab::builder()
+        .personal_token(token)
+        .build()
+        .context("failed to initialize GitHub API client with Octocrab library")?;
 
-    let response: serde_json::Value = github_client
+    let raw_response: serde_json::Value = github_client
         .graphql("
         query RepoStats {
-            pullRequests: repository(name: \"todolist-api-go\", owner: \"Awesome-Demo-App\") {
+            pullRequests: repository(name: \"todolistqwe-api-go\", owner: \"Awesome-Demo-App\") {
               all: pullRequests {
                 totalCount
               }
@@ -57,20 +61,20 @@ async fn main() -> octocrab::Result<()> {
               }
             }
           }")
-        .await?;
+        .await.context("failed to query GitHub GrrphQL API")?;
 
-    match response.as_object() {
-        Some(map) => match map.get("data") {
-            Some(map) => {
-                match Deserialize::deserialize(map) {
-                    Ok::<RepoMetrics, _>(repometrics) => println!("{:?}", repometrics),
-                    Err(_) => println!("LOL FAILED3"),
-                }
-            },
-            None => println!("LOL FAILED1"),
-        }
-        None => println!("LOL FAILED2"),
-    }
+    let response = raw_response.as_object().context("failed to interpret GitHub GraphQL API answer as a Map")?;
+
+    // if response.contains_key("errors") {
+    //     eprintln!("GraphQL query returned errors: {:?}", response.get("errors"));
+    //     Err(())
+    // }
+    let repo_metrics: RepoMetrics = Deserialize::deserialize(
+            response
+            .get("data")
+            .context("failed to find 'data' key inside response")?
+        ).context("failed to deserialize GraphQL query answer")?;
+    println!("{:?}", repo_metrics);
 
     Ok(())
 }
