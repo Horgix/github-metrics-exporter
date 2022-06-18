1c3b45cb779c7df825a00626742d012136ef63d8
Alexis "Horgix" Chotard <alexis.horgix.chotard@gmail.com>
2022-06-18 14:14:09 +0200

    ♻️ refactor(pr-metrics): extract pull requests metrics struct and (de)serialization helpers to their own file

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,14 @@
 use octocrab::Octocrab;
-use serde::{de, Serialize, Serializer, Deserialize, Deserializer};
-use anyhow::{Context, Result};
-use chrono::{DateTime, FixedOffset, Duration};
+use serde::{Serialize, Deserialize};
+use anyhow::{Context};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct PullRequestsMetrics {
-    #[serde(deserialize_with = "deserialize_nested_total_count")]
-    all: i32,
-    #[serde(deserialize_with = "deserialize_nested_total_count")]
-    open: i32,
-    #[serde(deserialize_with = "deserialize_duration_since_creation_date")]
-    #[serde(serialize_with = "serialize_duration")]
-    oldest: Duration,
-}
+mod pull_requests_metrics;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct RepoMetrics {
     #[serde(alias = "pullRequests")]
-    pull_requests: PullRequestsMetrics,
+    pull_requests: pull_requests_metrics::PullRequestsMetrics,
 }
-
-fn deserialize_nested_total_count<'de, D: Deserializer<'de>>(deserializer: D) -> Result<i32, D::Error> {
-    #[derive(Serialize, Deserialize, Debug)]
-    struct TotalCount {
-        #[serde(alias = "totalCount")] 
-        total_count: i32,
-    }
-
-    let res: TotalCount = de::Deserialize::deserialize(deserializer)?;
-    Ok(res.total_count)
-}
-
-// Deserializes a nested `createdAt` info from a GitHub GraphQL query response
-// into a Duration since this time
-// Example of input:
-//  "oldest": {
-//    "edges": [
-//      {
-//        "node": {
-//          "createdAt": "2021-12-31T25:59:59Z",
-//          "url": "https://github.com/SomeOwner/SomeRepo/pull/42"
-//        }
-//      }
-//    ]
-//  }
-fn deserialize_duration_since_creation_date<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Duration, D::Error> {
-    let answer: serde_json::Map<String, serde_json::Value> = de::Deserialize::deserialize(deserializer)?;
-    let edges = answer
-        .get("edges")
-        .expect("could not find `edges`")
-        .as_array()
-        .expect("could not interpret `edges` as an array");
-
-    if edges.len() != 1 {
-        // TODO
-    }
-    let raw_created_at: &str = edges[0]
-        .as_object().expect("failed to interpret first edge as a Map")
-        .get("node").expect("failed to find node in first edge")
-        .as_object().expect("failed to intepret node of first edge as a Map")
-        .get("createdAt").expect("failed to find `createdAt` in node")
-        .as_str().expect("failed to interpret `createdAt` as a String");
-
-    println!("{:?}", raw_created_at);
-
-    let created_at: DateTime<FixedOffset> = DateTime::parse_from_rfc3339(raw_created_at).unwrap();
-    println!("{:?}", created_at);
-    let duration_since_created_at: Duration = created_at.signed_duration_since(chrono::Utc::now());
-    Ok(duration_since_created_at)
-}
-
-pub fn serialize_duration<S>(
-    duration: &Duration,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let s = format!("{} days", duration.num_days().abs());
-    serializer.serialize_str(&s)
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let token = std::env::var("GITHUB_TOKEN").expect("environment variable 'GITHUB_TOKEN' is required");
@@ -94,7 +22,7 @@ async fn main() -> anyhow::Result<()> {
     let raw_response: serde_json::Value = github_client
         .graphql("
         query RepoStats {
-            pullRequests: repository(name: \"todolistqwe-api-go\", owner: \"Awesome-Demo-App\") {
+            pullRequests: repository(name: \"todolist-api-go\", owner: \"Awesome-Demo-App\") {
               all: pullRequests {
                 totalCount
               }
